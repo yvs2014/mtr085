c9a4aa783983cab3eb3fe4d61f47c2240cef5636
yvs <VSYakovetsky@gmail.com>
2023-05-29 18:36:29 +0300

    add debian packaging files

---
+++ build-deb.sh
@@ -0,0 +1,39 @@
+:
+
+# build depends on: dpkg-dev devscripts debhelper [libidn2]
+chk_cmd() {
+  command -v "$1" >/dev/null && return
+  echo "FAIL: '$1' is mandatory for packaging, please install '$2'"
+  exit 1
+}
+chk_cmd dpkg-buildpackage dpkg-dev
+chk_cmd dch devscripts
+chk_cmd dh debhelper
+
+set -e
+rev="$(git rev-list HEAD | sed '/^97af563/q' | sed -n '$=')"
+arch="$(dpkg-architecture -qDEB_BUILD_ARCH)"
+dist="$(lsb_release -cs)"
+name="mtr085"
+ddir="debs"
+nra="$ddir/${name}_${rev}_$arch"
+chf="debian/changelog"
+
+mkdir -p "$ddir"
+rm -f "$chf.tmp"
+dch --create -c "$chf.tmp" --package="$name" -v "$rev" -D "$dist" -u "low" -M \
+  "$name with IDN support, Unicode histograms, extra IP address info"
+
+mv "$chf.tmp" "$chf"
+
+bi_file="$nra.buildinfo"
+ch_file="$nra.changes"
+dpkg-buildpackage --help | grep -q buildinfo-file && \
+  BOUT="--buildinfo-file=$bi_file" COUT="--changes-file=$ch_file" || \
+  BOUT="--buildinfo-option=-O$bi_file" COUT="--changes-option=-O$ch_file" DH_OPTIONS="--destdir=$ddir"
+
+dpkg-buildpackage -b -tc --no-sign \
+  --buildinfo-option="-u$ddir" $BOUT \
+  --changes-option="-u$ddir" $COUT && \
+  (echo "\nPackages in $ddir/:"; ls -l "$ddir")
+
