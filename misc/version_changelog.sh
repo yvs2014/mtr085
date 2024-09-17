30d0e3064c3c2d570fd2e21c2c9b3f628fa11002
yvs <VSYakovetsky@gmail.com>
2024-09-18 00:43:12 +0300

    tuneup cmake building

--- misc/version_changelog.sh
+++ misc/version_changelog.sh
@@ -17,7 +17,7 @@ git_comments=
 for m in "$@"; do
 	git_comments="$git_comments -m \"$m\""
 done
-deb_comments='  * mtr085 with IDN support, Unicode histograms, extra IP address info, etc.'
+deb_comments='  * mtr085 with whois info, IDN support, unicode, etc.'
 
 vers="$(git rev-list --count $TAG0..HEAD)"
 next=$(($vers + 1))
